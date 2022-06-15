1e98f56bcd4c679f9ea7bfb3605b38c8e044b0b3
jabbar-dev <rashdijabbar@gmail.com>
2022-06-15 23:05:40 +0500

    creates environment variable

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const app = require('./app')
-
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const http = require('http')
 const server = http.createServer(app);
+require('dotenv').config();
 
 //Connect to MongoDB ATLAS
-mongoose.connect("mongodb+srv://Jabbar-dev:GQwvOmhWJgwtSHEX@cluster0.ny21o2w.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URL,
+{useNewUrlParser:true}
+
+).then(()=>{
+    console.log("Connected to MongoDB Atlas")
+}).catch(()=>{
+
+    console.log("Something Wrong With Database Connection")
+
+})
 
 
 server.listen(3000, console.log("Server is In the Air"));
