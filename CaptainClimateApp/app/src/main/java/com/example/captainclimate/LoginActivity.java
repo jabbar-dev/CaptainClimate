3e5d7792a6ea4c0e3193504d4b3987dc848a17f6
jabbar-dev <rashdijabbar@gmail.com>
2022-06-16 14:49:49 +0500

    Adds Login Page and Design

---
+++ CaptainClimateApp/app/src/main/java/com/example/captainclimate/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.captainclimate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
+}
--- CaptainClimateApp/app/src/main/java/com/example/captainclimate/MainActivity.java
+++ CaptainClimateApp/app/src/main/java/com/example/captainclimate/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.captainclimate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+
+
+
+        },3000);
     }
+
 }
