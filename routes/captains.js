fd6dd71780fefdaa21f209d41f0cfb630c76f406
jabbar-dev <rashdijabbar@gmail.com>
2022-06-16 15:40:21 +0500

    Adds Get Captain By Id REST API Update

--- routes/captains.js
+++ routes/captains.js
@@ -38,7 +38,7 @@ router.get("/:capId",(req, res)=>{
 
     Captain.findById(req.params.capId).then(captain=>{
         if(captain) res.send(captain);
-        res.status(404).send("Captain Not Found")
+        res.status(404).json.send("Captain Not Found")
     })
     .catch((error)=>{
         res.status(500).send(error.message)
