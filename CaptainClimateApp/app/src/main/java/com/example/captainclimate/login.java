596819eddb3d3f1303e38f134525441b223eae6a
jabbar-dev <rashdijabbar@gmail.com>
2022-06-16 12:53:39 +0500

    adds Login Activity

---
+++ CaptainClimateApp/app/src/main/java/com/example/captainclimate/login.java
@@ -0,0 +1,29 @@
+package com.example.captainclimate;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.captainclimate.databinding.ActivityLoginBinding;
+
+public class login extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
